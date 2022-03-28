310376d94184ebc3dacf9c6fcdce6b981fcca0af
mahmudunnaby <mahmudunnabyemon0@gmail.com>
2022-03-28 13:28:05 +0600

    Assignment Complete

--- src/components/Questions/Question1.js
+++ src/components/Questions/Question1.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Question1 = () => {
     return (
         <div>
-            <h1>How React Works?</h1>
-            <p> React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in the application, and React will efficiently update and render just the right component when your data changes.React Recates a virtual DOM and changes the dome using diff algorithm</p>
+            <h1 className='question'>How React Works?</h1>
+            <p> React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in the application, and React will efficiently update and render just the right component when your data changes.React creates a virtual DOM and changes the dome using diff algorithm</p>
             <h1>what is the difference between props and state</h1>
             <h3>Props</h3>
             <p>Props are read-only and immutable.Props can be accessed by the child component.It is used to communicate between components.The stateless component can have props it makes component reusable. </p>
